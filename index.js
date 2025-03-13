d12a8cd12801137d8a91dfc67519c7280a35c453
akshaykumarts <akshay.kumar846@gmail.com>
2025-03-14 00:41:32 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Kafka } = require('kafkajs');
+require('dotenv').config();
+
+const kafka = new Kafka({
+  clientId: 'tracking-producer-nestedJSON',
+  brokers: ['my-cluster-kafka-bootstrap.kafka.svc:9092'],
+  sasl: {
+    mechanism: "scram-sha-512",
+    username:  "4b0xm2bbnx2kvyw7l1ixbykmh", // Use env variable for security
+    password:  "GMbdA5pjNUNqoiX1BIYxRE4zQob8jZnT",
+  }
+});
+
+const producer = kafka.producer();
+const topic = process.env.KAFKA_TOPIC ;
+const key = 'static-key'; // Define key to avoid ReferenceError
+
+function getVehicleTrackingJson() {
+  return {
+      vehicleID: `VEHICLE-${Math.floor(Math.random() * 5) + 1}`,
+      speed: Math.floor(Math.random() * 120), // Random speed between 0-120 km/h
+      fuelLevel: Math.floor(Math.random() * 100), // Random fuel level between 0-100%
+      latitude: (Math.random() * 180 - 90).toFixed(6), // Random latitude
+      longitude: (Math.random() * 360 - 180).toFixed(6), // Random longitude
+      engineTemp: Math.floor(Math.random() * (120 - 70) + 70)
+  }
+}
+
+  async function sendMessage() {
+    await producer.connect();
+    console.log('Producer connected');
+    
+    setInterval(async () => {
+      const value = getVehicleTrackingJson();
+      await producer.send({
+        topic,
+        messages: [
+          { key, value: JSON.stringify(value) }
+        ]
+      });
+      console.log('Message sent:', value);
+    }, 3000); // Sending message every 3 seconds
+  }
+  
+  sendMessage().catch(console.error);
